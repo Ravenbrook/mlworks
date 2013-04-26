22093562ea468b0a3885cfc997ab4a86aaba6fe9
Richard Brooksby <rptb1+github@pobox.com>
2013-04-26 23:55:31 +0100

    Modifying Harlequin copyright notices on all source files to Ravenbrook copyright and open source license.

--- tools/mkstruct.bat
+++ tools/mkstruct.bat
@@ -51,7 +51,31 @@ rem Revision 1.1  1995/03/16  12:39:05  jont
 rem new unit
 rem NT version
 rem
-rem Copyright Harlequin Ltd. 1995
+rem Copyright 2013 Ravenbrook Limited <http://www.ravenbrook.com/>.
+rem All rights reserved.
+rem 
+rem Redistribution and use in source and binary forms, with or without
+rem modification, are permitted provided that the following conditions are
+rem met:
+rem 
+rem 1. Redistributions of source code must retain the above copyright
+rem    notice, this list of conditions and the following disclaimer.
+rem 
+rem 2. Redistributions in binary form must reproduce the above copyright
+rem    notice, this list of conditions and the following disclaimer in the
+rem    documentation and/or other materials provided with the distribution.
+rem 
+rem THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+rem IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+rem TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+rem PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+rem HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+rem SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+rem TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+rem PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+rem LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+rem NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+rem SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 cd MLW\src
 if not exist mir mkdir mir
 if not exist machine mkdir machine
